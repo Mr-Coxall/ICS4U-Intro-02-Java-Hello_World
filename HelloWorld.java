69f9b58f841ef8eb1a804fa5e16f01daa837a0c2
Patrick Coxall <mr.coxall@mths.ca>
2021-02-03 17:11:17 +0000

    fixed linting errors

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,10 +4,10 @@
 *
 * @author  Mr Coxall
 * @version 1.0
-* @since   2020-01-01 
+* @since   2020-01-01
 */
-class HelloWorld {
-    public static void main(String args[]){
+public final class HelloWorld {
+    public static void main(String args[]) {
      System.out.println("Hello, World!");
     }
 }
