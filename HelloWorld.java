56c3de8f6b2d475b254a228b0b9ec857738022b4
Patrick Coxall <mr.coxall@mths.ca>
2021-02-03 17:18:23 +0000

    fixed readme.md file

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,7 +8,7 @@
 */
 public final class HelloWorld {
     /**
-    * This method prints out Hello, World!
+    * This method prints out Hello, World! to the console
     */
     public static void main(String args[]) {
      System.out.println("Hello, World!");
