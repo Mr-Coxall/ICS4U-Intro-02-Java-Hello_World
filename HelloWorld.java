fcf6ed48e1d1ed72ce225cd8189237ccac958000
Patrick Coxall <mr.coxall@mths.ca>
2021-02-04 01:07:14 +0000

    Lint

--- HelloWorld.java
+++ HelloWorld.java
@@ -19,7 +19,7 @@ final class HelloWorld {
     * This method prints out "Hello, World!".
     * @param args
     */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println("Hello, World!");
     }
 }
